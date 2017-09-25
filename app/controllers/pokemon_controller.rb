fa6432830dacd82d79d9ea4d388710cb0c354a75
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-25 14:43:01 -0400

    added the Pokemon Info

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,26 @@
 class PokemonController < ApplicationController
 
   def index
+
+    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/pikachu/")
+
+    body = JSON.parse(response.body)
+
+    puts body["name"] # should be "pikachu"
+
     render json: {"message": "ok"}
+  end
+
+  def show
+    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(response.body)
+    name  = body["name"]
+
+    render json: {
+      "id":25,
+      "name":"pikachu",
+      "types":["electric"]
+    }
+
   end
 end
